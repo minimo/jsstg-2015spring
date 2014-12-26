d1fece22167c5f7f27dc69a9487c06c0f4d8052f
minimo <minimo.mobile@gmail.com>
2014-12-26 18:17:58 +0900

    前作からファイルコピー

---
+++ src/assets.js
@@ -0,0 +1,22 @@
+/*
+ *  assets.js
+ *  2014/07/10
+ *  @auther minimo  
+ *  This Program is MIT license.
+ */
+(function() {
+
+//アセット一覧
+tds.assets = {
+    //images
+
+    //SE
+    "explodeSmall": "assets/sen_ge_taihou03.mp3", 
+    "explodeLarge": "assets/sen_ge_hasai01.mp3", 
+    "explodePlayer": "assets/ta_ta_zuban_d01.mp3", 
+
+    //BGM
+    "warning":  "assets/bgm_warning.mp3",
+}
+
+})();
